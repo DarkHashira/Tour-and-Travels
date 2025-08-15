bd1368b5183c6fd84a43eb459bbd458bf2bfbf73
Arnab Das <darnab628@gmail.com>
2025-08-16 00:45:24 +0530

    Changed image pathing to absolute and routed cards to package info pages

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Cards.css'; // Assuming you have a CSS file for styling
 
 
@@ -8,16 +9,16 @@ const Cards = () =>{
         <div className="container cards">
                     <div className="row">
                         <div className="col-sm-12 col-md-8 col-lg-4 card1" >
-                            <img src="images/cards/goa.jpg" alt="...." height={'250px'} width={'100%'}></img>
-                            <a href="https://goa-tourism-guide-a9d9eg3.gamma.site/">Trip to Goa by SARASH</a>
+                            <img src="/images/cards/goa.jpg" alt="...." height={'250px'} width={'100%'}></img>
+                            <Link to="/info/goa">Trip to Goa by SARASH</Link>
                         </div>
                         <div className="col-sm-12 col-md-8 col-lg-4 card1" >
-                            <img src="images/cards/kashmir.jpg" alt="...." height={'250px'} width={'100%'}></img>
-                            <a href="https://explore-kashmir-wjakj2j.gamma.site/" >Trip to Kashmir by SARASH</a>
+                            <img src="/images/cards/kashmir.jpg" alt="...." height={'250px'} width={'100%'}></img>
+                            <Link to="/info/kashmir">Trip to Kashmir by SARASH</Link>
                         </div>
                         <div className="col-sm-12 col-md-8 col-lg-4 card1" >
-                            <img src="images/cards/delhi.jpg" alt="...." height={'250px'} width={'100%'}></img>
-                            <a href="https://discover-delhi-cm5qf7g.gamma.site/">Trip to Delhi by SARASH</a>
+                            <img src="/images/cards/delhi.jpg" alt="...." height={'250px'} width={'100%'}></img>
+                            <Link to="/info/delhi">Trip to Delhi by SARASH</Link>
                         </div>
                     </div>
                 </div>
